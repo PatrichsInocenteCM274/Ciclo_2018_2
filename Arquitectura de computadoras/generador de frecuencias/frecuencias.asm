ac222f554a46a279a91911d755b76fcd29a3df77
Patrichs Inocente <inocentepatrichs@gmail.com>
2018-10-02 00:12:38 -0500

    actaulizando

---
+++ Arquitectura de computadoras/generador de frecuencias/frecuencias.asm
@@ -0,0 +1,27 @@
+;RECORDAR QUE SOLO EL MODO 2 ES CON CARGA DE AUTO-RECARGA AUTOMATICA!!
+org 8000h
+mov TMOD,#21h ;
+ ; como un temporizador de 8 bits con auto recarga
+mov TH0,#1fh ;valor de recarga
+mov TL0,#0h
+setb TR0 ;inicia el timer 0
+espera_desborde:
+mov TH0,#1fh ;valor de recarga
+mov TL0,#0h
+	jnb TF0,$ 
+	clr TF0
+	mov TH0,#1fh ;valor de recarga
+mov TL0,#0h
+	jnb TF0,$ 
+	clr TF0
+	mov TH0,#1fh ;valor de recarga
+mov TL0,#0h
+	jnb TF0,$ 
+	clr TF0
+	mov TH0,#1fh ;valor de recarga
+mov TL0,#0h
+	jnb TF0,$ 
+	clr TF0
+	cpl P1.0
+	sjmp espera_desborde
+end
